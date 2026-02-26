e9ee08123e5164bf1f82166780557433dd508c5a
Onyebuchukwu Anthony <onyebuchukwumontana@gmail.com>
2026-02-26 11:25:32 -0800

    Initial commit

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,13 +1,16 @@
 require("dotenv").config();
+require("@nomicfoundation/hardhat-toolbox");
 
 module.exports = {
   solidity: "0.8.20",
 
   networks: {
     bsctestnet: {
-      url: "https://data-seed-prebsc-1-s1.binance.org:8545",
+      url: "https://data-seed-prebsc-1-s1.binance.org:8545/",
       chainId: 97,
-      accounts: [process.env.PRIVATE_KEY]
-    }
-  }
+      accounts: process.env.PRIVATE_KEY
+        ? [process.env.PRIVATE_KEY]
+        : [],
+    },
+  },
 };
